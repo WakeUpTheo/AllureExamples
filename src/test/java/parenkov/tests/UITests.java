34dda868d3800d3a8f8a322e61a6eff6180d3064
ParenkovLocal <parenkov.fedor@gmail.com>
2022-01-25 18:03:19 +0400

    Small edits

--- src/test/java/parenkov/tests/UITests.java
+++ src/test/java/parenkov/tests/UITests.java
@@ -1,6 +1,11 @@
 package parenkov.tests;
 
-import io.qameta.allure.*;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Owner;
+import io.qameta.allure.AllureId;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
